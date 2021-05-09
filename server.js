61b4a036503a3221b0dfc78c1c2a81f3c6533b52
mlemke24 <mattrlemke@gmail.com>
2021-05-09 17:09:37 -0500

    refactored server

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-const sequelize = require('./config/connection');
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
